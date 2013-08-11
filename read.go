8bcadfa902b3adbcc67108115f281fbd7c2e618b
Mathieu Turcotte <turcotte.mat@gmail.com>
2013-08-11 10:54:58 -0400

    Move the parsing logic to a separate file.

---
+++ read.go
@@ -0,0 +1,168 @@
+// Copyright (c) 2013 Mathieu Turcotte
+// Licensed under the MIT license.
+
+package sourcemap
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+)
+
+const maxEntryValue = 5    // Maximum number of values within a single entry.
+const lineSeparator = ';'  // Line separator in the "mappings" field.
+const entrySeparator = ',' // Entry separator withing a line in the "mappings" field.
+
+const defaultNumLines = 256  // Default number of lines to allocate per source map.
+const defaultNumEntries = 16 // Default number of entries to allocate per line.
+
+// A struct into which the source map JSON representation is unmarshalled.
+type jsonSourceMap struct {
+	Version        int
+	File           string
+	SourceRoot     string
+	Sources        []string
+	SourcesContent []string
+	Names          []string
+	Mappings       string
+}
+
+// Reads a source map.
+func Read(reader io.Reader) (s SourceMap, err error) {
+	var jsonMap jsonSourceMap
+	var lines []Line
+
+	dec := json.NewDecoder(reader)
+	if err = dec.Decode(&jsonMap); err != nil {
+		return
+	}
+
+	if jsonMap.Version != 3 {
+		err = fmt.Errorf("unsupported version: %v", s.Version)
+	}
+
+	if lines, err = parseMappings(jsonMap.Mappings); err != nil {
+		return
+	}
+
+	// Populate the source map structure.
+	s.Version = jsonMap.Version
+	s.File = jsonMap.File
+	s.SourceRoot = jsonMap.SourceRoot
+	s.Sources = jsonMap.Sources
+	s.SourcesContent = jsonMap.SourcesContent
+	s.Names = jsonMap.Names
+	s.Mappings = lines
+
+	return
+}
+
+// A struct to hold the state while parsing the mappings.
+type mappingsParseState struct {
+	gencol int // 0-based index of the generated column.
+	source int // 0-based index into the "sources" list.
+	line   int // 0-based starting line in the original source.
+	column int // 0-based starting column of the line in the source.
+	name   int // 0-based index into the "names" list.
+}
+
+func parseMappings(data string) (lines []Line, err error) {
+	lines = make([]Line, 0, defaultNumLines)
+	reader := strings.NewReader(data)
+	state := mappingsParseState{}
+	line := make(Line, 0, defaultNumEntries)
+	for reader.Len() > 0 {
+		if consume(reader, lineSeparator) {
+			lines = append(lines, line)
+			line = make(Line, 0, defaultNumEntries)
+			state.gencol = 0
+		} else {
+			i := 0
+			var values [maxEntryValue]int
+			for !entryCompleted(reader) {
+				if val, derr := decodeVQL(reader); derr != nil {
+					err = derr
+					return
+				} else {
+					values[i] = val
+					i++
+				}
+			}
+
+			entry, eerr := newEntry(&state, values, i)
+			if eerr != nil {
+				err = eerr
+				return
+			}
+
+			line = append(line, entry)
+			consume(reader, entrySeparator)
+		}
+	}
+
+	return
+}
+
+// Attempts to consume the wanted character from the reader. Returns true if
+// character was read successfully, false otherwise. If the character could
+// not be read, then no characters are consumed from the reader.
+func consume(r *strings.Reader, wanted byte) bool {
+	if r.Len() > 0 {
+		b, _ := r.ReadByte()
+		if b == wanted {
+			return true
+		}
+		r.UnreadByte()
+	}
+	return false
+}
+
+// Looks ahead to see if the next character completes the entry. Does not read
+// any characters from the reader.
+func entryCompleted(r *strings.Reader) bool {
+	if r.Len() > 0 {
+		b, _ := r.ReadByte()
+		r.UnreadByte()
+		return b == lineSeparator || b == entrySeparator
+	}
+	return true
+}
+
+func newEntry(s *mappingsParseState, values [maxEntryValue]int,
+	numValues int) (e Entry, err error) {
+	e = Entry{-1, -1, -1, -1, -1}
+
+	switch numValues {
+	case 1:
+		// Unmapped section of the generated file.
+		e.GeneratedColumn = values[0] + s.gencol
+		s.gencol = e.GeneratedColumn
+	case 4:
+		// Mapped section of the generated file.
+		e.GeneratedColumn = values[0] + s.gencol
+		e.SourceFileId = values[1] + s.source
+		e.SourceLine = values[2] + s.line
+		e.SourceColumn = values[3] + s.column
+		s.gencol = e.GeneratedColumn
+		s.source = e.SourceFileId
+		s.line = e.SourceLine
+		s.column = e.SourceColumn
+	case 5:
+		// Mapped section of the generated file with an associated name.
+		e.GeneratedColumn = values[0] + s.gencol
+		e.SourceFileId = values[1] + s.source
+		e.SourceLine = values[2] + s.line
+		e.SourceColumn = values[3] + s.column
+		e.NameId = values[4] + s.name
+		s.gencol = e.GeneratedColumn
+		s.source = e.SourceFileId
+		s.line = e.SourceLine
+		s.column = e.SourceColumn
+		s.name = e.NameId
+	default:
+		err = fmt.Errorf("unexpected number of values in entry: %v", numValues)
+	}
+
+	return
+}
--- sourcemap.go
+++ sourcemap.go
@@ -6,16 +6,9 @@
 package sourcemap
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"sort"
-	"strings"
 )
-
-const maxEntryValue = 5    // Maximum number of values within a single entry.
-const lineSeparator = ';'  // Line separator in the "mappings" field.
-const entrySeparator = ',' // Entry separator withing a line in the "mappings" field.
 
 // Represents the mapping to a line/column/name in the original file.
 type OriginalMapping struct {
@@ -24,6 +17,20 @@ type OriginalMapping struct {
 	Column int
 	Name   string
 }
+
+// Represents a section of the generated source that can be mapped back to the
+// original source.
+type Entry struct {
+	GeneratedColumn int // 0-based column of this entry in the generated source.
+	SourceFileId    int // Index of the source file in the "sources" list.
+	SourceLine      int // 0-based line number of this entry in the source file.
+	SourceColumn    int // 0-based column number of this entry in the source file.
+	NameId          int // Index of the symbol name in the "names" list.
+}
+
+// Represents a line in the generated source. A line is composed of entries
+// containing information about the original source file.
+type Line []Entry
 
 // A struct representing the decoded source map.
 type SourceMap struct {
@@ -34,17 +41,6 @@ type SourceMap struct {
 	SourcesContent []string
 	Names          []string
 	Mappings       []Line
-}
-
-// A struct into which the source map JSON data is unmarshalled.
-type jsonSourceMap struct {
-	Version        int
-	File           string
-	SourceRoot     string
-	Sources        []string
-	SourcesContent []string
-	Names          []string
-	Mappings       string
 }
 
 // Given a line and a column number in the generated code, find a mapping in
@@ -108,150 +104,4 @@ func (s *SourceMap) populateMapping(mapping *OriginalMapping, entry Entry) {
 	if entry.NameId >= 0 {
 		mapping.Name = s.Names[entry.NameId]
 	}
-}
-
-// Represents a section of the generated source that can be mapped back to the
-// original source.
-type Entry struct {
-	GeneratedColumn int // 0-based column of this entry in the generated source.
-	SourceFileId    int // Index of the source file in the "sources" list.
-	SourceLine      int // 0-based line number of this entry in the source file.
-	SourceColumn    int // 0-based column number of this entry in the source file.
-	NameId          int // Index of the symbol name in the "names" list.
-}
-
-// Represents a line in the generated source. A line is composed of entries
-// containing information about the original source file.
-type Line []Entry
-
-// Struct to hold the state while parsing the "mappings" field.
-type unmarshalState struct {
-	gencol int // 0-based index of the generated column.
-	source int // 0-based index into the "sources" list.
-	line   int // 0-based starting line in the original source.
-	column int // 0-based starting column of the line in the source.
-	name   int // 0-based index into the "names" list.
-}
-
-func parseMappings(data string) (lines []Line, err error) {
-	lines = make([]Line, 0, 256)
-	reader := strings.NewReader(data)
-	state := unmarshalState{}
-	line := make(Line, 0, 10)
-	for reader.Len() > 0 {
-		if consume(reader, lineSeparator) {
-			lines = append(lines, line)
-			line = make(Line, 0, 10)
-			state.gencol = 0
-		} else {
-			i := 0
-			var values [maxEntryValue]int
-			for !entryCompleted(reader) {
-				if val, derr := decodeVQL(reader); derr != nil {
-					err = derr
-					return
-				} else {
-					values[i] = val
-					i++
-				}
-			}
-
-			entry, eerr := newEntry(&state, values, i)
-			if eerr != nil {
-				err = eerr
-				return
-			}
-
-			line = append(line, entry)
-			consume(reader, entrySeparator)
-		}
-	}
-
-	return
-}
-
-func consume(r *strings.Reader, wanted byte) bool {
-	if r.Len() > 0 {
-		b, _ := r.ReadByte()
-		if b == wanted {
-			return true
-		}
-		r.UnreadByte()
-	}
-	return false
-}
-
-func entryCompleted(r *strings.Reader) bool {
-	if r.Len() > 0 {
-		b, _ := r.ReadByte()
-		r.UnreadByte()
-		return b == lineSeparator || b == entrySeparator
-	}
-	return true
-}
-
-func newEntry(s *unmarshalState, values [maxEntryValue]int,
-	numValues int) (e Entry, err error) {
-	e = Entry{-1, -1, -1, -1, -1}
-
-	switch numValues {
-	case 1:
-		// Unmapped section of the generated file.
-		e.GeneratedColumn = values[0] + s.gencol
-		s.gencol = e.GeneratedColumn
-	case 4:
-		// Mapped section of the generated file.
-		e.GeneratedColumn = values[0] + s.gencol
-		e.SourceFileId = values[1] + s.source
-		e.SourceLine = values[2] + s.line
-		e.SourceColumn = values[3] + s.column
-		s.gencol = e.GeneratedColumn
-		s.source = e.SourceFileId
-		s.line = e.SourceLine
-		s.column = e.SourceColumn
-	case 5:
-		// Mapped section of the generated file with an associated name.
-		e.GeneratedColumn = values[0] + s.gencol
-		e.SourceFileId = values[1] + s.source
-		e.SourceLine = values[2] + s.line
-		e.SourceColumn = values[3] + s.column
-		e.NameId = values[4] + s.name
-		s.gencol = e.GeneratedColumn
-		s.source = e.SourceFileId
-		s.line = e.SourceLine
-		s.column = e.SourceColumn
-		s.name = e.NameId
-	default:
-		err = fmt.Errorf("unexpected number of values in entry: %v", numValues)
-	}
-
-	return
-}
-
-// Reads a source map.
-func Read(reader io.Reader) (s SourceMap, err error) {
-	var jsonMap jsonSourceMap
-	dec := json.NewDecoder(reader)
-	if err = dec.Decode(&jsonMap); err != nil {
-		return
-	}
-
-	if jsonMap.Version != 3 {
-		err = fmt.Errorf("unsupported version: %v", s.Version)
-	}
-
-	lines, err := parseMappings(jsonMap.Mappings)
-	if err != nil {
-		return
-	}
-
-	s.Version = jsonMap.Version
-	s.File = jsonMap.File
-	s.SourceRoot = jsonMap.SourceRoot
-	s.Sources = jsonMap.Sources
-	s.SourcesContent = jsonMap.SourcesContent
-	s.Names = jsonMap.Names
-	s.Mappings = lines
-
-	return
 }
